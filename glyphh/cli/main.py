9b523c44472c8a6bc5a209117df77c4a67a6dd6e
Christopher Timmerman <christopher.timmerman@boomi.com>
2026-02-19 19:44:12 -0600

    fix: use RUNTIME_RELEASES_TOKEN for all checkout steps
    release: v0.3.9

--- glyphh/cli/main.py
+++ glyphh/cli/main.py
@@ -15,7 +15,7 @@ from .commands.serve import serve_command
 
 
 @click.group(invoke_without_command=True)
-@click.version_option(version="0.3.8", prog_name="glyphh")
+@click.version_option(version="0.3.9", prog_name="glyphh")
 @click.pass_context
 def cli(ctx):
     """
