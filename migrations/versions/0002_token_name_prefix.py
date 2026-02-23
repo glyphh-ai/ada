844eda04795be91d6db69b5e36bc01b2937b068e
Christopher Timmerman <ctimmetim@gmail.com>
2026-02-23 07:04:46 -0600

    fix: alembic migration 0002 references wrong down_revision
    down_revision was '0001' but the actual revision ID is
    '0001_initial'. Alembic couldn't resolve the chain, causing
    KeyError: '0001' on startup.

--- migrations/versions/0002_token_name_prefix.py
+++ migrations/versions/0002_token_name_prefix.py
@@ -1,14 +1,14 @@
 """Add name and token_prefix columns to tokens table.
 
 Revision ID: 0002
-Revises: 0001
+Revises: 0001_initial
 """
 
 from alembic import op
 import sqlalchemy as sa
 
 revision = "0002"
-down_revision = "0001"
+down_revision = "0001_initial"
 branch_labels = None
 depends_on = None
 
